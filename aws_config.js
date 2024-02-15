b77089069c29c4a9a37245bcd2548648c185af24
Jack Dewey <jack.a.dewey@gmail.com>
2024-02-15 00:40:14 -0800

    started aws integration

---
+++ aws_config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    aws_table_name: 'rio_characters',
+    aws_local_config: {
+      //Provide details for local configuration here
+    },
+    aws_remote_config: {
+         accessKeyId: "Your accessKeyId here",
+         secretAccessKey: "Your secretAccessKey here",
+         region: 'us-west-2',
+    }
+};
